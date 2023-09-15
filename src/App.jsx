413d1cd5ef2a4f98b99d355f942dddb5736c65e1
Mike Criollo <mkcriollo@gmail.com>
2023-09-15 14:32:09 -0400

    Create a Search Form for Pets / A result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom";
-import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="chicken" />
-      <Pet name="Liter" animal="cat" breed="mixed" />
+      <SearchParams />
     </div>
   );
 };
